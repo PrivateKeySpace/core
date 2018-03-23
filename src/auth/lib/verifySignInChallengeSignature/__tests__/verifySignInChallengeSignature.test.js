5f4213b0d04c960863143c6bce20b1faf126733c
Valentyn Barmashyn <barmashyn.val@gmail.com>
2018-03-23 19:31:51 +0200

    PKSP-29 naming fixes

--- src/auth/lib/verifySignInChallengeSignature/__tests__/verifySignInChallengeSignature.test.js
+++ src/auth/lib/verifySignInChallengeSignature/__tests__/verifySignInChallengeSignature.test.js
@@ -1,24 +1,19 @@
 const verifySignInChallengeSignature = require('../verifySignInChallengeSignature')
 const { validSignInData, invalidSignInData } = require('../__mocks__/signInData')
 
-function verifySignInPayload (payload) {
+function verifySignInData (payload) {
   const { signature, publicKey, challenge, implementation } = payload
-  return verifySignInChallengeSignature(
-    signature,
-    publicKey,
-    challenge,
-    implementation
-  )
+  return verifySignInChallengeSignature(signature, publicKey, challenge, implementation)
 }
 
 it('should return true with correctly provided signature, public key, visual and hidden challenges and trezor implementation', () => {
-  for (const challenge of validSignInData) {
-    expect(verifySignInPayload(challenge)).toBe(true)
+  for (const payload of validSignInData) {
+    expect(verifySignInData(payload)).toBe(true)
   }
 })
 
 it('should return false with invalid signature/public key/visual and hidden challenges/trezor implementation', () => {
-  for (const challenge of invalidSignInData) {
-    expect(verifySignInPayload(challenge)).toBe(false)
+  for (const payload of invalidSignInData) {
+    expect(verifySignInData(payload)).toBe(false)
   }
 })
